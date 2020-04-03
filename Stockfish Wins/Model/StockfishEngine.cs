7e3c2eae2a850363919c80290524add137c4f50e
karims <karims@uw.edu>
2020-04-02 18:00:09 -0700

    fixed indentation

--- Stockfish Wins/Model/StockfishEngine.cs
+++ Stockfish Wins/Model/StockfishEngine.cs
@@ -14,10 +14,12 @@ namespace ChessGame_PI_FinalProject
     public class StockfishEngine : ChessEngine
     {
         private Process chessEngine = new Process();
+
         public StockfishEngine()
         {
             StartProcess("stockfish_10_x32.exe");
         }
+
         public void StartProcess(String processPath)
         {
             chessEngine.StartInfo.FileName = processPath;
@@ -26,6 +28,7 @@ namespace ChessGame_PI_FinalProject
             chessEngine.StartInfo.RedirectStandardOutput = true;
             chessEngine.StartInfo.UseShellExecute = false;
         }
+
         public String GetComputerMove(List<String> MoveOrderInUCINotation)
         {
             chessEngine.Start();
@@ -33,6 +36,7 @@ namespace ChessGame_PI_FinalProject
             string processReply2 = chessEngine.StandardOutput.ReadToEnd();
             return MoveExtractor.ExtractMove(processReply2);
         }
+
         private void InputMoveOrder(List<String> MoveOrderInUCINotation)
         {
             chessEngine.StandardInput.WriteLine("position startpos moves " + String.Join(" ", MoveOrderInUCINotation));
